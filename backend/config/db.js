7e1bd6fa4df1bd99ac927dec29fc21a25988e88e
User <user@Practos-MacBook-Pro.local>
2026-02-04 12:07:09 +0530

    chnages for deployment

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,16 +2,22 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-const promisePool = pool.promise();
+pool.getConnection((err) => {
+  if (err) {
+    console.error("MySQL connection failed:", err);
+  } else {
+    console.log("MySQL Connected ✅");
+  }
+});
 
-promisePool
-  .getConnection()
-  .then(() => console.log("MySQL Connected"))
-  .catch((err) => console.log("MySQL Error:", err.message));
-
-module.exports = promisePool;
+module.exports = pool.promise();
+ß
